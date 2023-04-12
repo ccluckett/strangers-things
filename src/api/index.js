0d0a551e85a20992317a073bb68957d8c04cca09
Christian McNeil <christianmcneil@christians-mbp.mynetworksettings.com>
2023-04-12 15:30:40 -0400

    Style register and login

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,80 @@
 const COHORT_NAME = "2303-ftb-et-web-ft";
 const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
+const fakeData = {
+  success: true,
+  error: null,
+  data: {
+    posts: [
+      {
+        location: "[On Request]",
+        willDeliver: false,
+        messages: [],
+        active: true,
+        _id: "642762a8cd3bfb001620064e",
+        author: {
+          _id: "642762a8cd3bfb0016200648",
+          username: "joe1234",
+        },
+        cohort: "642762a8cd3bfb0016200646",
+        title: "Practically new Stradivarius",
+        description:
+          "I've really only used this three or four times.  I thought it would be a good purchase, shows what I know.",
+        price: "$14.3 million",
+        createdAt: "2023-03-31T22:46:00.837Z",
+        updatedAt: "2023-03-31T22:46:00.880Z",
+        __v: 0,
+        isAuthor: false,
+      },
+      {
+        location: "[On Request]",
+        willDeliver: true,
+        messages: [],
+        active: true,
+        _id: "642762a8cd3bfb001620064f",
+        author: {
+          _id: "642762a8cd3bfb0016200649",
+          username: "jane1234",
+        },
+        cohort: "642762a8cd3bfb0016200646",
+        title: "Golden Retriever puppies",
+        description:
+          "Not looking for any money, just want to find a good home for these four beautiful pups.",
+        price: "free",
+        createdAt: "2023-03-31T22:46:00.837Z",
+        updatedAt: "2023-03-31T22:46:00.886Z",
+        __v: 0,
+        isAuthor: false,
+      },
+      {
+        location: "Ames, IA",
+        willDeliver: true,
+        messages: [],
+        active: true,
+        _id: "642762a8cd3bfb0016200650",
+        author: {
+          _id: "642762a8cd3bfb001620064a",
+          username: "caesar1234",
+        },
+        cohort: "642762a8cd3bfb0016200646",
+        title: "NordicTrack Freestrider Elliptical Trainer",
+        description: "To be honest, it is more amazing than my resolve.",
+        price: "$1400, OBO",
+        createdAt: "2023-03-31T22:46:00.837Z",
+        updatedAt: "2023-03-31T22:46:00.891Z",
+        __v: 0,
+        isAuthor: false,
+      },
+    ],
+  },
+};
+
 //Fetch Post
 export const fetchPosts = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/posts`);
-    const { data } = await response.json();
+    // const response = await fetch(`${BASE_URL}/posts`);
+    // const { data } = await response.json();
+    const { data } = fakeData;
     return data;
   } catch (error) {
     console.error(error);
@@ -36,19 +105,19 @@ export const registerUser = async (username, password) => {
 
 //Fetch Token
 
-export const login = async (username,password) => {
+export const login = async (username, password) => {
   try {
-    const response = await fetch(`${BASE_URL}/users/login`,{
+    const response = await fetch(`${BASE_URL}/users/login`, {
       method: "POST",
       headers: {
-        'Conent-Type': 'application/json'
+        "Conent-Type": "application/json",
       },
       body: JSON.stringify({
         user: {
           username: username,
           password: password,
-        }
-      })
+        },
+      }),
     });
     const result = await response.json();
     console.log(result);
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,35 +1,56 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { login } from "../api";
 import { useNavigate } from "react-router-dom";
 
-
-const SignIn = ({token,setToken,setIsLoggedIn}) => {
-  const navigate = useNavigate ()
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const SignIn = ({ token, setToken, setIsLoggedIn }) => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (event) => {
-      event.preventDefault();
-      const { token } = await login(username,password);
-      let response = token;
-      console.log (token)
-      if ( token === response ) {
-          setIsLoggedIn(true);
-          setToken(response);
-          navigate("/profile")
-      };
-  
+    event.preventDefault();
+    const { token } = await login(username, password);
+    let response = token;
+    console.log(token);
+    if (token === response) {
+      setIsLoggedIn(true);
+      setToken(response);
+      navigate("/profile");
+    }
   };
-  return(
+  return (
+    <div className="form-sigin w-50 mt-5 ms-auto me-auto">
+      <form onSubmit={handleSubmit}>
+        <div className="form-floating">
+          <input
+            className="form-control"
+            type="text"
+            value={username}
+            required
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <label for="floatingInput">Username</label>
+        </div>
+        <div className="form-floating">
+          <input
+            className="form-control"
+            type="text"
+            value={password}
+            required
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+          <label for="floatingInput">Password:</label>
+        </div>
 
-    <form onSubmit={handleSubmit}>
-      <label>Username:</label>
-      <input type="text" value={username} required onChange={(event) => setUsername(event.target.value)}></input>
-      <label>Password:</label>
-      <input type="text" value={password} required onChange={(event) => setPassword(event.target.value)}></input>
-      <button type="submit">Sign In</button>
-  
-    </form>
+        <button type="submit" className="btn btn-outline-success w-100 mt-3">
+          Sign In
+        </button>
+      </form>
+    </div>
   );
 };
 
